c426865aeb1afef4cd3377bb47931c4a2a7f2516
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-08-13 09:04:02 +0600

    Learing Javascript basic

---
+++ Lecture-12/script.js
@@ -0,0 +1,9 @@
+// arithmetic operator
+const fristNumber = 10;
+const lastNumber = 5;
+
+console.log(`${fristNumber} + ${lastNumber} = ${fristNumber + lastNumber}`);
+console.log(`${fristNumber} - ${lastNumber} = ${fristNumber - lastNumber}`);
+console.log(`${fristNumber} * ${lastNumber} = ${fristNumber * lastNumber}`);
+console.log(`${fristNumber} / ${lastNumber} = ${fristNumber / lastNumber}`);
+console.log(`${fristNumber} % ${lastNumber} = ${fristNumber % lastNumber}`);
