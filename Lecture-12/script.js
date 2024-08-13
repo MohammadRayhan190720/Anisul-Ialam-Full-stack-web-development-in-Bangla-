b7c8bc62b6698f289e1a28a28f643f65a67bdcf2
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-08-13 09:17:37 +0600

    Learning Operator

--- Lecture-12/script.js
+++ Lecture-12/script.js
@@ -1,6 +1,8 @@
 // arithmetic operator
-const fristNumber = 10;
-const lastNumber = 5;
+// const fristNumber = 10;
+// const lastNumber = 5;
+const fristNumber = Number(prompt("Enter FristNumber:"));
+const lastNumber = Number(prompt("Enter lastNumber:"));
 
 console.log(`${fristNumber} + ${lastNumber} = ${fristNumber + lastNumber}`);
 console.log(`${fristNumber} - ${lastNumber} = ${fristNumber - lastNumber}`);
